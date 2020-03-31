a9b4dd6d05166d0179ea39b8ff19481c753f527e
ejmvizmonte <62785545+ejmvizmonte@users.noreply.github.com>
2020-03-31 15:26:39 +0800

    Answers

--- ems/answer.php
+++ ems/answer.php
@@ -12,9 +12,33 @@
 	$age = $_GET['age'];
 	$food = $_GET['food'];
 	
+	if ($age > 50) {
+		$age_answer = "You're old!";
+	} else {
+		$age_answer = "No comment.";
+	}
+	
+	switch ($food) {
+		
+		case "adobo" :
+			$food_answer = "Yummy!";
+			break;
+			
+		case "asado" :
+			$food_answer = "Why not adobo?";
+			break;
+			
+		default:
+			$food_answer = "Anything but adobo.";
+			break;
+
+	}
+		
 	echo '<p>Your name is: '.$name.'</p>';
 	echo '<p>Your age is: '.$age.'</p>';
+	echo '<p>' .$age_answer. '</p>';
 	echo '<p>Your favorite food is: '.$food.'</p>';
+	echo '<p>' .$food_answer. '</p>';
 ?>
 
 </body>
