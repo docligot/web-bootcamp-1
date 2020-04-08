2c7e58c8c0c1e73b19cff96376960b6bc20bf507
Dominic Ligot <dominic.ligot@cirrolytix.com>
2020-04-09 04:27:20 +0800

    Updated parameters

--- doc/db_practice/script.js
+++ doc/db_practice/script.js
@@ -16,7 +16,7 @@ function getData() {
 					dataString2.push(dataArray[i][series2]);
 				}
 				createTable(series1, series2, labels, dataString1, dataString2);
-				drawChart(labels, dataString1, dataString2, 'line')
+				drawChart(series1, series2, labels, dataString1, dataString2, 'line')
 			} else {
 				document.getElementById('output').innerHTML = "Loading...";
 			}
@@ -36,44 +36,94 @@ function createTable(series1, series2, labels, dataString1, dataString2) {
 	document.getElementById('output').innerHTML = output;	
 }
 
-function drawChart(labels, dataString1, dataString2, chart_type) {
+function loadColors() {
+    window.chartColors = {
+		black: 'rgba(128, 128, 128, 1)',
+		white: 'rgba(255, 255, 255, 1)',
+		red: 'rgba(255, 128, 128, 1)',
+		lime: 'rgba(128, 255, 128, 1)',
+		blue: 'rgba(128, 128, 255, 1)',
+		yellow: 'rgba(255, 255, 128, 1)',
+		cyan: 'rgba(128, 255, 255, 1)',
+		magenta: 'rgba(255, 128, 255, 1)',
+		ltblack: 'rgba(128, 128, 128, 0.2)',
+		ltwhite: 'rgba(255, 255, 255, 0.2)',
+		ltred: 'rgba(255, 128, 128, 0.2)',
+		ltlime: 'rgba(128, 255, 128, 0.2)',
+		ltblue: 'rgba(128, 128, 255, 0.2)',
+		ltyellow: 'rgba(255, 255, 128, 0.2)',
+		ltcyan: 'rgba(128, 255, 255, 0.2)',
+		ltmagenta: 'rgba(255, 128, 255, 0.2)'    
+		};
+}
+
+function drawChart(series1, series2, labels, dataString1, dataString2, chart_type) {
 	
-	document.getElementById('chart-container').innerHTML = '<canvas id="myChart"></canvas>';
+	document.getElementById('chart-container').innerHTML = '<canvas id="myChart" style="height:250px;"></canvas>';
 	
 	var name = JSON.parse('[' + labels + ']');
 	var data1 = JSON.parse('[' + dataString1 + ']');
 	var data2 = JSON.parse('[' + dataString2 + ']');
 	
+	loadColors();
+	
 	var chartdata = {
 		labels: name, 
 		datasets: [
 			{
-			label: 'Users', 
-			backgroundColor: 'rgba(255, 99, 132, 0.2)',
-			borderColor: 'rgba(255,99,132,1)', 
+			label: series1, 
+			backgroundColor: window.chartColors.ltblue,
+			borderColor: window.chartColors.blue, 
 			data: data1, 
-			borderWidth: 2
+			borderWidth: 2,
+			yAxisID: 'y1'
 			}, 
 			{
-			label: 'Sessions', 
-			backgroundColor: 'rgba(54, 162, 235, 0.2)',
-			borderColor: 'rgba(54, 162, 235, 1)', 
+			label: series2, 
+			backgroundColor: window.chartColors.ltred,
+			borderColor: window.chartColors.red, 
 			data: data2, 
-			borderWidth: 2
+			borderWidth: 2,
+			yAxisID: 'y2'
 			}
 		]
 	};
-	var ctx = document.getElementById("myChart");
+	var ctx = document.getElementById("myChart").getContext('2d');
 	var myChart = new Chart(ctx, {
 		type: chart_type,
 		data: chartdata, 
 		options: {
+			responsive: true,
+			maintainAspectRatio: false,
 			scales: {
-				yAxes: [{
-						ticks: {
-							min: 0
-						}
-					}]
+                yAxes: [{
+					type: 'linear', 
+					display: true, 
+					position: 'left', 
+					id: 'y1', 
+					scaleLabel : {
+						display: true, 
+						labelString: series1
+					}, 
+                    ticks: {
+                        beginAtZero: true
+                    }					
+				}, {
+					type: 'linear', 
+					display: true, 
+					position: 'right', 
+					id: 'y2', 
+					scaleLabel : {
+						display: true, 
+						labelString: series2
+					}, 
+                    ticks: {
+                        beginAtZero: true
+                    }, 
+					gridLines: {
+						display: false 
+					}
+				}] 
 				}, 
 			legend: {
 				position: 'bottom', 
