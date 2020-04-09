914265555222ddfb477269480f0cebcafc21bc91
Dominic Ligot <dominic.ligot@cirrolytix.com>
2020-04-09 16:59:34 +0800

    Updated table

--- doc/db_chart/script.js
+++ doc/db_chart/script.js
@@ -3,7 +3,7 @@ function getCharts1() {
 	var series4 = document.getElementById('series4').value;
 	var series5 = document.getElementById('series5').value;
 	var series6 = document.getElementById('series6').value;	
-	if (series4 && series5 && series6) {
+	if (series4 && series5 && series6 && series7) {
 		extractData1(series4, series5, series6);
 	}
 }
@@ -22,11 +22,11 @@ function extractData1(series4, series5, series6) {
 				dataSeries1.push(dataArray1[i][series4]);
 				dataSeries2.push(dataArray1[i][series5]);
 			}
-
 			console.log(labels);
 			console.log(dataSeries1);
 			console.log(dataSeries2);
 			drawChart1(labels, dataSeries1, dataSeries2, series4, series5);
+			createTable(labels, dataSeries1, dataSeries2, series4, series5);
 
 		} 
 	};
@@ -39,6 +39,8 @@ function drawChart1(data1, data2, data3, legend1, legend2) {
 	var labels = JSON.parse('[' + data1 + ']');
 	var dataSeries1 = JSON.parse('[' + data2 + ']');
 	var dataSeries2 = JSON.parse('[' + data3 + ']');
+	var chartType = document.getElementById('series7').value;
+	document.getElementById('chart-container-1').innerHTML = '<canvas id="myChart1"></canvas>';
 	
 	window.chartColors = {
 		red: 'rgb(255, 99, 132)',
@@ -54,7 +56,7 @@ function drawChart1(data1, data2, data3, legend1, legend2) {
 	var ctx = document.getElementById('myChart1').getContext('2d');
 	var chart = new Chart(ctx, {
 		// The type of chart we want to create
-		type: 'line',
+		type: chartType,
 
 		// The data for our dataset
 		data: {
@@ -66,7 +68,7 @@ function drawChart1(data1, data2, data3, legend1, legend2) {
 					backgroundColor: window.chartColors.orange,
 					borderColor: window.chartColors.orange,
 					borderWidth: 2, 
-					yAxesID: 'y1', 
+					yAxisID: 'y1', 
 					data: dataSeries1
 				},
 				{
@@ -76,7 +78,7 @@ function drawChart1(data1, data2, data3, legend1, legend2) {
 					borderColor: window.chartColors.black,
 					borderWidth: 2, 
 					steppedLine: true,
-					yAxesID: 'y2', 
+					yAxisID: 'y2', 
 					data: dataSeries2
 				}
 			]
@@ -94,14 +96,35 @@ function drawChart1(data1, data2, data3, legend1, legend2) {
 					ticks: {beginAtZero: true},
 					type: 'linear', 
 					position: 'left', 
-					id: 'y1'
+					id: 'y1', 
+					scaleLabel: {
+						display: true, 
+						labelString: legend1
+					}
 				}, {
 					ticks: {beginAtZero: true},
 					type: 'linear', 
 					position: 'right', 
-					id: 'y2'
+					id: 'y2',
+					scaleLabel: {
+						display: true, 
+						labelString: legend2
+					}
 				}]
 			}
 		}
 	})
 }
+
+function createTable(labels, dataSeries1, dataSeries2, legend1, legend2) {
+	var tableOutput = '<table class="w3-table w3-striped">';
+	tableOutput += '<tr class="w3-black"><th>Periods</th><th>' + legend1 + '</th><th>' + legend2 + '</th></tr>';
+	for (var i in labels) {
+		tableOutput += '<tr><td>' + labels[i] + '</td><td>' + dataSeries1[i] + '</td><td>' + dataSeries2[i] + '</td></tr>'; 		
+	}
+	tableOutput += '</table>';
+	document.getElementById('table-container').innerHTML = tableOutput; 
+}
+
+
+
