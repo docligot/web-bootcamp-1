e2c663e69b4987a05f0f780428f12aea74a28a78
ejmvizmonte <62785545+ejmvizmonte@users.noreply.github.com>
2020-04-09 15:01:57 +0800

    revisedchart

--- ems/db2/call_chart.php
+++ ems/db2/call_chart.php
@@ -15,29 +15,27 @@ $dbc = pg_connect('host=' .$host. ' port=' .$port. ' dbname=' .$dbname. ' user='
 
 $query = "SELECT period, $series4, $series5 FROM ems.full_data_view";
 
-switch ($series6) {
-	case "Train" :
-		$query .= " WHERE \"set\" = 'Train'";
-		break;
-	case "Test" :
-		$query .= " WHERE \"set\" = 'Test'";
-		break;
-	default :
-		break;
-}
-
 $result = pg_query($dbc, $query);
+
+		$result_string['labels'] = '';
+		$result_string[$series4] = '';
+		$result_string[$series5] = '';
 
 if ($result) {
 
 	while ($row = pg_fetch_array($result)) {
-		$result_string[] = $row;
+		$result_string['labels'] .= ','.$row['period'];
+		$result_string[$series4] .= ','.$row[$series4];
+		$result_string[$series5] .= ','.$row[$series5];
 	}
+		$result_string['labels'] = ltrim($result_string['labels'], ',');
+		$result_string[$series4] = ltrim($result_string[$series4], ',');
+		$result_string[$series5] = ltrim($result_string[$series5], ',');
+	
 	echo json_encode($result_string);
+	
 }	else {
 		echo '';
 }
 
-pg_close($dbc);
-exit();
 /*echo json_encode($result);*/
