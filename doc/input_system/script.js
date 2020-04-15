679a53db5fac52ee676c5443d21aff10b64d9e99
Dominic Ligot <dominic.ligot@cirrolytix.com>
2020-04-15 14:04:06 +0800

    Update script.js

--- doc/input_system/script.js
+++ doc/input_system/script.js
@@ -3,21 +3,16 @@ function checkErrors() {
 	var product = document.getElementById('product').value;
 	var price = document.getElementById('price').value;
 	var quantity = document.getElementById('quantity').value;
-	
 	if (product == '') {
 		errors += 'Please select a product.<br/>';
 	}
-	
 	if (price == '' || price == 0 || price < 0) {
 		errors += 'Please enter a valid price.<br/>';
 	}
-	
 	if (quantity == '' || quantity == 0 || quantity < 0) {
 		errors += 'Please enter a valid quantity.<br/>';
 	}
-	
 	return errors;
-	
 }
 
 function calculateTotal() {
@@ -27,7 +22,6 @@ function calculateTotal() {
 		var quantity = document.getElementById('quantity').value;		
 		document.getElementById('totalBox').innerHTML = quantity * price;
 	} else {
-		document.getElementById('totalBox').innerHTML = '&nbsp';
+		document.getElementById('totalBox').innerHTML = '&nbsp;';
 	}
-
 }
