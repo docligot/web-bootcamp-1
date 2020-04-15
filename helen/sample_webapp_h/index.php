12055444a1507f46fea5a4b02b3e7462871b9800
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-04-15 15:01:55 +0800

    Added 2-column responsive and W3 styling to the sections

--- helen/sample_webapp_h/index.php
+++ helen/sample_webapp_h/index.php
@@ -7,9 +7,34 @@
 
 </head>
 
-<link rel="stylesheet" href="styling.css" />
+<!--<link rel="stylesheet" href="styling.css" />-->
+<link rel="stylesheet" href="w3.css" />
 
 <body>
+	<header class="w3-top w3-padding w3-indigo">
+		<div class="w3-large">H Sample Web App</div>
+	</header>
+
+	<section>
+		<img  src="virus.png" style="width: 100%"; max-height=300px; />
+	</section>
+
+	<section class="w3-padding-large w3-center">
+		<h1>This is a sample website</h1>
+	</section>
+
+	<section class="w3-padding-large">
+		<div class="w3-row">
+		<div><p class="w3-half">Lorem ipsum keme keme keme 48 years quality control ng at bakit urky kirara urky tungril tamalis lulu chapter oblation shonget makyonget pinkalou otoko at bakit ganders waz jongoloids bakit sa at nang otoko jongoloids sudems at bakit na mashumers kasi lulu 48 years at nang chopopo na ang krang-krang chipipay keme Gino wiz Mike ano wrangler bakit bakit buya jongoloids warla bakit at ang ganda lang pamentos thunder shala ano katagalugan makyonget jutay pranella at nang at bakit at bakit at bakit katagalugan jowabella sangkatuts pranella ng ugmas ano sa shokot nang shonga shonget sudems pinkalou bella Cholo na ang neuro antibiotic ano at nang krang-krang makyonget pamin daki antibiotic na na ang otoko chuckie na jupang-pang jowabelles bakit at nang at bakit jowa cheapangga.</p></div>
+
+		<div><p class="w3-half">Lorem ipsum keme keme keme 48 years quality control ng at bakit urky kirara urky tungril tamalis lulu chapter oblation shonget makyonget pinkalou otoko at bakit ganders waz jongoloids bakit sa at nang otoko jongoloids sudems at bakit na mashumers kasi lulu 48 years at nang chopopo na ang krang-krang chipipay keme Gino wiz Mike ano wrangler bakit bakit buya jongoloids warla bakit at ang ganda lang pamentos thunder shala ano katagalugan makyonget jutay pranella at nang at bakit at bakit at bakit katagalugan jowabella sangkatuts pranella ng ugmas ano sa shokot nang shonga shonget sudems pinkalou bella Cholo na ang neuro antibiotic ano at nang krang-krang makyonget pamin daki antibiotic na na ang otoko chuckie na jupang-pang jowabelles bakit at nang at bakit jowa cheapangga.</p></div>
+		</div>
+	</section>
+
+	<footer class="w3-bottom w3-black w3-padding">
+		<div>&copy; Cirrolytix</div>
+	</footer>
+
 	<h1 id="main-header">This is a website.</h1>
 	<h2 style="color:red;">This is a heading</h2>
 	<h2 class="blue-header">This is a heading</h2>
@@ -20,9 +45,9 @@
 	<h2>This is a heading</h2>
 	<h2>This is a heading</h2>
 	
-	<p class="blue-header">Lorem ipsum keme keme keme 48 years quality control ng at bakit urky kirara urky tungril tamalis lulu chapter oblation shonget makyonget pinkalou otoko at bakit ganders waz jongoloids bakit sa at nang otoko jongoloids sudems at bakit na mashumers kasi lulu 48 years at nang chopopo na ang krang-krang chipipay keme Gino wiz Mike ano wrangler bakit bakit buya jongoloids warla bakit at ang ganda lang pamentos thunder shala ano katagalugan makyonget jutay pranella at nang at bakit at bakit at bakit katagalugan jowabella sangkatuts pranella ng ugmas ano sa shokot nang shonga shonget sudems pinkalou bella Cholo na ang neuro antibiotic ano at nang krang-krang makyonget pamin daki antibiotic na na ang otoko chuckie na jupang-pang jowabelles bakit at nang at bakit jowa cheapangga.</p>
+	
 
-	<p>Lorem ipsum keme keme keme 48 years quality control ng at bakit urky kirara urky tungril tamalis lulu chapter oblation shonget makyonget pinkalou otoko at bakit ganders waz jongoloids bakit sa at nang otoko jongoloids sudems at bakit na mashumers kasi lulu 48 years at nang chopopo na ang krang-krang chipipay keme Gino wiz Mike ano wrangler bakit bakit buya jongoloids warla bakit at ang ganda lang pamentos thunder shala ano katagalugan makyonget jutay pranella at nang at bakit at bakit at bakit katagalugan jowabella sangkatuts pranella ng ugmas ano sa shokot nang shonga shonget sudems pinkalou bella Cholo na ang neuro antibiotic ano at nang krang-krang makyonget pamin daki antibiotic na na ang otoko chuckie na jupang-pang jowabelles bakit at nang at bakit jowa cheapangga.</p>
+	
 
 </body>
 
