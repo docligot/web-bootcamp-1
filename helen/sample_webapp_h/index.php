5b7bbe486264e73b55989780603c858e888b6c4b
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-04-15 15:22:27 +0800

    Added javascript event for hidden section with button

--- helen/sample_webapp_h/index.php
+++ helen/sample_webapp_h/index.php
@@ -29,6 +29,13 @@
 
 		<div><p class="w3-half w3-padding">Lorem ipsum keme keme keme 48 years quality control ng at bakit urky kirara urky tungril tamalis lulu chapter oblation shonget makyonget pinkalou otoko at bakit ganders waz jongoloids bakit sa at nang otoko jongoloids sudems at bakit na mashumers kasi lulu 48 years at nang chopopo na ang krang-krang chipipay keme Gino wiz Mike ano wrangler bakit bakit buya jongoloids warla bakit at ang ganda lang pamentos thunder shala ano katagalugan makyonget jutay pranella at nang at bakit at bakit at bakit katagalugan jowabella sangkatuts pranella ng ugmas ano sa shokot nang shonga shonget sudems pinkalou bella Cholo na ang neuro antibiotic ano at nang krang-krang makyonget pamin daki antibiotic na na ang otoko chuckie na jupang-pang jowabelles bakit at nang at bakit jowa cheapangga.</p></div>
 		</div>
+		<div>
+		<button class="w3-button w3-blue" onclick="show_hidden();">Toggle Hidden</button>
+		</div>
+	</section>
+
+	<section id="hidden-section" class="w3-padding-large w3-center w3-black" style="display:none;">
+		<div class="w3-xxlarge">This is the hidden section</div>
 	</section>
 
 		<section class="w3-grey w3-padding-large">
@@ -55,6 +62,22 @@
 	<footer class="w3-bottom w3-black w3-padding">
 		<div>&copy; Cirrolytix</div>
 	</footer>
+
+
+<!-- Use the ID hidden-section to show here -->
+	<script>
+		function show_hidden() {
+			var hidden_section=document.getElementById('hidden-section').style.display;
+			if(hidden_section=='none') {
+				document.getElementById('hidden-section').style.display='block';
+			}
+			else {
+				document.getElementById('hidden-section').style.display='none';
+			}
+
+		}
+
+	</script>
 </body>
 
 </html>
