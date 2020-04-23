bcd3af591d9e31786e6cccf9401e5a055cf3774d
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-04-23 19:02:57 +0800

    Tested mac include functionality

--- helen/analytics/index.php
+++ helen/analytics/index.php
@@ -21,11 +21,31 @@
 	
 	<nav class="quattrocento">
 		<div class="w3-sidebar ambient w3-col l2 w3-bar-block w3-hide-medium w3-hide-small">
-			<? include('menu.php'); ?>
+			//<? include('menu.php'); ?>
+
+
+<a href="#section1" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-traffic-light" style="margin-right: 10px;"></i>Indicators</div></a>
+<a href="#section2" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-stream" style="margin-right: 10px;"></i>Progress</div></a>
+<a href="#section3"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-line" style="margin-right: 10px;"></i>Chart</div></a>
+<a href="#section4"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share" style="margin-right: 10px;"></i>Journey</div></a>
+<a href="#section5"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share-alt" style="margin-right: 10px;"></i>Activity Network</div></a>
+<a href="#section6"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-map-marked-alt" style="margin-right: 10px;"></i>Maps</div></a>
+<a href="#section7"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fas fa-quote-right" style="margin-right: 10px;"></i>Text Analysis</div></a>
+<a href="#about"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-bar" style="margin-right: 10px;"></i>About</div></a>
+
 		</div>
 
 		<div id="navBar" class="w3-sidebar ambient w3-animate-left w3-bar-block w3-hide w3-large">
-			<? include('menu.php'); ?>
+			//<? include('menu.php'); ?>
+
+<a href="#section1" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-traffic-light" style="margin-right: 10px;"></i>Indicators</div></a>
+<a href="#section2" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-stream" style="margin-right: 10px;"></i>Progress</div></a>
+<a href="#section3"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-line" style="margin-right: 10px;"></i>Chart</div></a>
+<a href="#section4"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share" style="margin-right: 10px;"></i>Journey</div></a>
+<a href="#section5"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share-alt" style="margin-right: 10px;"></i>Activity Network</div></a>
+<a href="#section6"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-map-marked-alt" style="margin-right: 10px;"></i>Maps</div></a>
+<a href="#section7"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fas fa-quote-right" style="margin-right: 10px;"></i>Text Analysis</div></a>
+<a href="#about"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-bar" style="margin-right: 10px;"></i>About</div></a>
 		</div>
 	</nav>
 
