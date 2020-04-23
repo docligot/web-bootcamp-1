2c05f35dc11b8476b4ee6d40076ff47e138edd44
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-04-23 13:27:37 +0800

    Added last database connection portion for transaction table

---
+++ helen/input_system/transactionTable.php
@@ -0,0 +1,14 @@
+<?php
+include('dbConnect.php');
+$query = 'SELECT * FROM helen.product_input ORDER BY period;';
+$result = pg_query($dbc, $query); 
+if ($result) {
+	while ($row = pg_fetch_array($result)) {
+		$result_string[] = $row;
+	}
+	echo json_encode($result_string);
+} else {
+	echo "There was an error.";
+}
+pg_close($dbc);
+exit();
