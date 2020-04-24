4adda41e7eb355b7a47ffb644b8cddc1481f9816
lswh <labao.hm@gmail.com>
2020-04-24 09:21:36 +0800

    Added php on each php line

--- helen/analytics/index.php
+++ helen/analytics/index.php
@@ -21,31 +21,11 @@
 	
 	<nav class="quattrocento">
 		<div class="w3-sidebar ambient w3-col l2 w3-bar-block w3-hide-medium w3-hide-small">
-			//<? include('menu.php'); ?>
-
-
-<a href="#section1" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-traffic-light" style="margin-right: 10px;"></i>Indicators</div></a>
-<a href="#section2" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-stream" style="margin-right: 10px;"></i>Progress</div></a>
-<a href="#section3"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-line" style="margin-right: 10px;"></i>Chart</div></a>
-<a href="#section4"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share" style="margin-right: 10px;"></i>Journey</div></a>
-<a href="#section5"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share-alt" style="margin-right: 10px;"></i>Activity Network</div></a>
-<a href="#section6"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-map-marked-alt" style="margin-right: 10px;"></i>Maps</div></a>
-<a href="#section7"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fas fa-quote-right" style="margin-right: 10px;"></i>Text Analysis</div></a>
-<a href="#about"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-bar" style="margin-right: 10px;"></i>About</div></a>
-
+			<?php include('menu.php'); ?>
 		</div>
 
 		<div id="navBar" class="w3-sidebar ambient w3-animate-left w3-bar-block w3-hide w3-large">
-			//<? include('menu.php'); ?>
-
-<a href="#section1" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-traffic-light" style="margin-right: 10px;"></i>Indicators</div></a>
-<a href="#section2" onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-stream" style="margin-right: 10px;"></i>Progress</div></a>
-<a href="#section3"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-line" style="margin-right: 10px;"></i>Chart</div></a>
-<a href="#section4"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share" style="margin-right: 10px;"></i>Journey</div></a>
-<a href="#section5"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-share-alt" style="margin-right: 10px;"></i>Activity Network</div></a>
-<a href="#section6"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-map-marked-alt" style="margin-right: 10px;"></i>Maps</div></a>
-<a href="#section7"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fas fa-quote-right" style="margin-right: 10px;"></i>Text Analysis</div></a>
-<a href="#about"onclick="toggleClose('navBar');"><div class="hover-ambient w3-bar-item"><i class="fas fa-chart-bar" style="margin-right: 10px;"></i>About</div></a>
+			<?php include('menu.php'); ?>
 		</div>
 	</nav>
 
@@ -124,35 +104,35 @@
 			<section id="section3" class="w3-padding">
 				<div class="w3-xlarge" >Chart</div>
 				<p>Standard bar, line, scatterplot graphs can be displayed powered by data from a database.</p>
-				<br/><? include ('chart/chart_sample.html'); ?>
+				<br/><?php include ('chart/chart_sample.html'); ?>
 				<hr/>
 			</section>			
 
 			<section id="section4" class="w3-padding">
 				<div class="w3-xlarge" >Customer Journey Flow</div>
 				<p>A sankey or flow diagram shows sequences of events and common paths from event data.</p>
-				<br/><? include ('sankey/index.html'); ?>
+				<br/><?php include ('sankey/index.html'); ?>
 				<hr/>
 			</section>	
 
 			<section id="section5" class="w3-padding">
 				<div class="w3-xlarge" >Activity Network</div>
 				<p>A network diagram shows inter-relationships between entities, items, and concepts and shows clustering of ideas.</p>
-				<br/><? include ('network/network2.html'); ?>
+				<br/><?php include ('network/network2.html'); ?>
 				<hr/>
 			</section>	
 
 			<section id="section6" class="w3-padding">
 				<div class="w3-xlarge" >Maps</div>
 				<p>Locate places based on longitude and latitude values.</p>
-				<br/><? include ('maps/index.html'); ?>
+				<br/><?php include ('maps/index.html'); ?>
 				<hr/>
 			</section>	
 
 			<section id="section7" class="w3-padding">
 				<div class="w3-xlarge" >Text Analysis</div>
 				<p>Inspect word relationships.</p>
-				<br/><? include ('wordcloud/index.html'); ?>
+				<br/><?php include ('wordcloud/index.html'); ?>
 				<hr/>
 			</section>	
 
---
+++ helen/analytics/test.php
@@ -0,0 +1,6 @@
+
+<?php
+// won't work, evaluated as include(('vars.php') == TRUE), i.e. include('')
+include('menu.php');
+?>
+
