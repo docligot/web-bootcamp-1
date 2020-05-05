631bd79863cc047f1027f4e51fd69f5c0a19117d
kristelzapata <44916803+kristelzapata@users.noreply.github.com>
2020-05-05 14:26:46 +0800

    updated folders

---
+++ Kristel/python_web/get.py
@@ -0,0 +1,14 @@
+import cgi
+
+data = cgi.FieldStorage()
+vafriable = data.getValue('variable')
+
+print('Content-type:text/html\r\n\r\n')
+print('Hello World')
+print('<!DOCTYPE HTML>')
+print('<html>')
+print('<head>')
+print('<title>The Python response</title></head>')
+print('<body>')
+print('<h1>You selected:', variable,'</h1>')
+print('</body></html>')
