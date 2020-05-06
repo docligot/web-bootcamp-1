c80f398ca16c6c35b4c1aea91a39789bd965ce04
tessa-tan <44916413+tessa-tan@users.noreply.github.com>
2020-05-07 02:31:20 +0800

    update python exercise

--- tessa/python_web_2/cluster.py
+++ tessa/python_web_2/cluster.py
@@ -7,10 +7,10 @@ from sklearn.metrics import silhouette_score
 data = cgi.FieldStorage()
 clusters = data.getvalue('clusters')
 
-xl = pd.ExcelFile("clustering_data.xlsx")
+xl = pd.ExcelFile(r"clustering_data.xlsx")
 df = xl.parse("Sheet1")
 train = df.drop(['record_id'], axis = 1)
-km = KMeans(n_clusters=int(clusters))
+km = KMeans(n_clusters=int(2))
 km.fit(train)
 print('Content-type:text/html \r\n\r\n')
 print(silhouette_score(km.fit_transform(train), km.labels_))
---
+++ tessa/python_web_2/post.py
@@ -0,0 +1,22 @@
+import cgi
+data = cgi.FieldStorage()
+textstring = data.getvalue('textString')
+numberentry = data.getvalue('numberEntry')
+
+def add10(x):
+	y = x + 10
+	return y
+
+print('Content-type:text/html \r\n\r\n')
+print('Hello World')
+print('<!DOCTYPE HTML>')
+print('<html>')
+print('<head>')
+print('<title>The Python response</title></head>')
+print('<body>')
+print('<p>You entered: ', textstring, '</p>')
+print('<p>This is the number you entered:</p>')
+print('<p>', numberentry, '</p>')
+print('<p>This is the number with the function applied:</p>')
+print('<p>', add10(int(numberentry)), '</p>')
+print('</body></html>')
