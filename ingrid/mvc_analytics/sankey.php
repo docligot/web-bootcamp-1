84be18660014f5a2b554c8010cc0cc2398f56246
Ingrid <ingrid.tanedo@cirrolytix.com>
2020-05-18 22:12:07 +0800

    sankey exercise

---
+++ ingrid/mvc_analytics/sankey.php
@@ -0,0 +1,14 @@
+<?
+
+function showSankey() {
+?>
+<div class="spacer"></div>
+<div class="w3-row">
+  <div></div>
+  <div></div>
+</div>
+<div class="w3-padding">This is the sankey page.</div>
+
+<?php
+}
+
