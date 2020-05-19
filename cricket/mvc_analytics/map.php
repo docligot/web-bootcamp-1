9f738e098e4a218e2bb1368144595e9b37bec71b
cricketsoong <cricket.soong@gmail.com>
2020-05-19 15:43:56 +0800

    updated my files
    copied bootcamp files

---
+++ cricket/mvc_analytics/map.php
@@ -0,0 +1,13 @@
+<?php
+
+function showMap() {
+?>
+<div class="w3-row">
+	<div class="w3-col l2">&nbsp;</div>
+	<div class="w3-col l10 w3-padding">This is the map page.</div>
+</div>
+
+
+
+<?php
+}
