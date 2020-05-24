0f4c348918c52a7c8581e1e5dd6c6c8c4687d229
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-24 19:02:36 +0800

    Played with size

--- helen/mvc_analytics/test/linesample.php
+++ helen/mvc_analytics/test/linesample.php
@@ -3,7 +3,7 @@
 function showChart() {
 ?>
 
-<canvas id="myChart" style="width:90%; height: 50%;"></canvas>
+<canvas id="myChart" style="width:50%; height: 50%;"></canvas>
 
 <script src="Chart.js" ></script>
 <script src="linesample.js"></script>
