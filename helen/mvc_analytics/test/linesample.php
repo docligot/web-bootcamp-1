3fcd703c9717d4b4d1682730825aaf0b286be19c
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-24 18:57:09 +0800

    Added localized ChartJS script for safety

--- helen/mvc_analytics/test/linesample.php
+++ helen/mvc_analytics/test/linesample.php
@@ -5,7 +5,7 @@ function showChart() {
 
 <canvas id="myChart" style="width:90%; height: 50%;"></canvas>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0" ></script>
+<script src="Chart.js" ></script>
 <script src="linesample.js"></script>
 
 <?php
