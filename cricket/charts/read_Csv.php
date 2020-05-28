39e00361ed2df5c3cc015e8f040b68a990d33954
cricketsoong <cricket.soong@gmail.com>
2020-05-29 07:04:15 +0800

    updates to charts
    implementation of generating arrays via
    PHP
    PHP from CSV
    Python creates JSON, which is read by JS

---
+++ cricket/charts/read_Csv.php
@@ -0,0 +1,12 @@
+<?php
+
+function extractFile($filename) {
+	$file = fopen($filename, "r");
+	while (! feof($file)) {
+		$result[] = (fgetcsv($file));
+	}
+	return json_encode($result);
+	fclose($file);
+}
+
+print_r(extractFile("data.csv"));
