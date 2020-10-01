da852cca7e885d1adf051fb64907382eebde5cc3
Dominic Ligot <dominic.ligot@gmail.com>
2020-10-01 17:28:05 +0800

    update web sample

---
+++ doc/answer.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = $_GET['name'];
+$age = $_GET['age'];
+$location = $_GET['location'];
+
+echo '<h2>Your name is ' . $name . '!</h2><br/>';
+echo '<h3>Your age is ' . $age . '!</h3><br/>';
+echo '<h3>Your location is ' . $location . '!</h3>';
+
+
+
+
