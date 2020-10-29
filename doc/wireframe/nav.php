5ca9de0164e9ae393dc41c35027cfe5f691c8ba8
Dominic Ligot <dominic.ligot@gmail.com>
2020-10-29 17:46:38 +0800

    update index php

---
+++ doc/wireframe/nav.php
@@ -0,0 +1,6 @@
+		<nav class="w3-col l2 w3-grey w3-sidebar w3-bar-block">
+			<a href="./?page=map"><div class="w3-bar-item w3-padding w3-button">Map</div></a>
+			<a href="./?page=sir"><div class="w3-bar-item w3-padding w3-button">SIR</div></a>
+			<a href="./?page=table"><div class="w3-bar-item w3-padding w3-button">Table</div></a>
+			<a href="./?page=word"><div class="w3-bar-item w3-padding w3-button">Word Network</div></a>
+		</nav>
