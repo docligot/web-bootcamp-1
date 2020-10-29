7ede92093f24819cab8c00304b22835bfdda1f58
Dominic Ligot <dominic.ligot@gmail.com>
2020-10-29 17:50:20 +0800

    update pages

--- doc/wireframe/index.php
+++ doc/wireframe/index.php
@@ -8,7 +8,6 @@
 </head>
 
 <body>
-
 	<header class="w3-top w3-blue w3-xlarge w3-padding">Top Bar</header>
 	<div style="height: 52px;"></div>
 	<div class="w3-row">
@@ -16,31 +15,12 @@
 		<div class="w3-col l2">&nbsp;</div>
 		<div class="w3-col l10">
 			<div class="w3-row">
-				<div class="w3-quarter w3-padding">
-					<p>Magni adipisci eos rerum et. Delectus voluptatibus asperiores est ea molestias ad tenetur repellat. Magnam tenetur voluptas animi modi assumenda accusantium est asperiores. Aut illo quos animi. Deleniti molestiae sapiente assumenda cum impedit necessitatibus explicabo totam. Rerum beatae dignissimos est dolore natus illum.</p>
-					<p>Nam explicabo quae debitis blanditiis quo voluptatem minima culpa. Quis recusandae quo voluptate debitis enim. Impedit dolores sequi maiores explicabo dolorem ut quas. Et cum voluptatibus odio tempora est eum hic dolores. Veritatis ut accusantium laborum illum. Error voluptatem accusamus minus a sed sint velit iusto.</p>
-				</div>
-				<div class="w3-threequarter w3-padding">
-					<img src="map.png" style="width: 100%;" />
-				</div>
+
+
+
 			</div>
 			<footer class="w3-bottom w3-black w3-padding">Footer</footer>
 		</div>
 	</div>
-
-
-
-	
-
-	<section></section>
-
-	<article></article>
-
-	<div></div>
-
-	<span></span>
-
-	
-
 </body>
 </html>
---
+++ doc/wireframe/sir_chart.php
@@ -0,0 +1,8 @@
+				<div class="w3-quarter w3-padding">
+					<h2>SIR Page</h2>
+					<p>Magni adipisci eos rerum et. Delectus voluptatibus asperiores est ea molestias ad tenetur repellat. Magnam tenetur voluptas animi modi assumenda accusantium est asperiores. Aut illo quos animi. Deleniti molestiae sapiente assumenda cum impedit necessitatibus explicabo totam. Rerum beatae dignissimos est dolore natus illum.</p>
+					<p>Nam explicabo quae debitis blanditiis quo voluptatem minima culpa. Quis recusandae quo voluptate debitis enim. Impedit dolores sequi maiores explicabo dolorem ut quas. Et cum voluptatibus odio tempora est eum hic dolores. Veritatis ut accusantium laborum illum. Error voluptatem accusamus minus a sed sint velit iusto.</p>
+				</div>
+				<div class="w3-threequarter w3-padding">
+					<img src="sir_chart.png" style="width: 100%;" />
+				</div>
