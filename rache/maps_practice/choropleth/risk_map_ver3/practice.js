5832c27c4f01d9f3558caa7b798328ef76e65d81
Rache <rachemelendres@gmail.com>
2020-12-25 17:17:21 +0800

    upload ph choropleths

---
+++ rache/maps_practice/choropleth/risk_map_ver3/practice.js
@@ -0,0 +1,235 @@
+		function load_json() {
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+					region = JSON.parse(this.responseText);
+					console.log(region);
+
+				}
+			};
+			xhttp.open("GET", "regional.json", true);
+			xhttp.send();
+		}
+
+		function load_risk() {
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+					risk = JSON.parse(this.responseText);
+					console.log(risk);
+				}
+			};
+			xhttp.open("GET", "extract_risk.php", true);
+			xhttp.send();
+		}
+		
+		function display_risk(value) {
+			if (value) {
+				for (var i in region.features) {
+					add_polygon(i, value);
+				}	
+			}
+		}
+		
+		// color palette for hazard
+		hazard_colors = [
+			"#feedde",
+			"#fdbe85",
+			"#fd8d3c",
+			"#e6550d",
+			"#a63603"
+		];
+		// color palette for vulnerability
+		vulnerability_colors = [
+			"#f1eef6",
+			"#bdc9e1",
+			"#74a9cf",
+			"#2b8cbe",
+			"#045a8d"
+		];
+		// color palette for lack of coping capacity
+		lack_res_colors = [
+			"#ffffcc",
+			"#c2e699",
+			"#78c679",
+			"#31a354",
+			"#006837"
+		];
+		
+		// color palette for covid risk
+		risk_colors = [
+			"#fee5d9",
+			"#fcae91",
+			"#fb6a4a",
+			"#de2d26",
+			"#a50f15"
+		];
+
+		load_json();
+		load_risk();
+		
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZG9jbGlnb3QiLCJhIjoiY2p3MHQ5MTViMGVvNzQzdGdicTlwM2o3NCJ9.j4qYChJYSxUy8hNnlXrD-g';
+        map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [121.056269, 14.569240],
+        zoom: 5
+        });
+				
+			
+				var hoveredStateId = null;
+        function add_polygon(area, risk_value) {
+			//console.log(region_colors[risk[area][risk_value]]);
+			
+			var sourceName = 'polygon'+area;
+			var layerName = region.features[area].properties.ADM1_EN;
+			var layerNameBorders = region.features[area].properties.ADM1ALT1EN;
+			var color =[];
+			var risk_indicator="";
+		
+			if (risk_value==1){
+					risk_indicator = "Hazard & Exposure"
+					if (risk[area][risk_value]<=19){
+									color = hazard_colors[0];
+					} else if (risk[area][risk_value]>19 && risk[area][risk_value]<=34){
+									color = hazard_colors[1];
+					} else if (risk[area][risk_value]>34 && risk[area][risk_value]<=49){
+									color = hazard_colors[2];
+					} else if (risk[area][risk_value]>49 && risk[area][risk_value]<=64) {
+									color = hazard_colors[3];
+					} else {
+									color = hazard_colors[4];
+					};
+				}
+			else if (risk_value ==2){
+				risk_indicator = "Vulnerability"
+					if (risk[area][risk_value]<=19){
+									color = vulnerability_colors[0];
+					} else if (risk[area][risk_value]>19 && risk[area][risk_value]<=34){
+									color = vulnerability_colors[1];
+					} else if (risk[area][risk_value]>34 && risk[area][risk_value]<=49){
+									color = vulnerability_colors[2];
+					} else if (risk[area][risk_value]>49 && risk[area][risk_value]<=64) {
+									color = vulnerability_colors[3];
+					} else {
+									color = vulnerability_colors[4];
+					};
+				}
+			else if (risk_value ==3){
+				risk_indicator = "Lack of Coping Capacity"
+					if (risk[area][risk_value]<=19){
+						color = lack_res_colors[0];
+					} else if (risk[area][risk_value]>19 && risk[area][risk_value]<=34){
+									color = lack_res_colors[1];
+					} else if (risk[area][risk_value]>34 && risk[area][risk_value]<=49){
+									color = lack_res_colors[2];
+					} else if (risk[area][risk_value]>49 && risk[area][risk_value]<=64) {
+									color = lack_res_colors[3];
+					} else {
+									color = lack_res_colors[4];
+					};
+			}
+			else {
+						risk_indicator = "COVID-19 Risk Index"
+						if (risk[area][risk_value]<=19){
+							color = risk_colors[0];
+						} else if (risk[area][risk_value]>19 && risk[area][risk_value]<=34){
+										color = risk_colors[1];
+						} else if (risk[area][risk_value]>34 && risk[area][risk_value]<=49){
+										color = risk_colors[2];
+						} else if (risk[area][risk_value]>49 && risk[area][risk_value]<=64) {
+										color = risk_colors[3];
+						} else {
+										color = risk_colors[4];
+						};
+				}
+			if (map.getSource(sourceName)) {
+				map.removeLayer(layerName);
+				map.removeLayer(layerNameBorders);
+				map.removeSource(sourceName);
+			}
+            map.addSource(sourceName, {
+                'type': 'geojson',
+                'data': {
+					'type': 'Feature',
+					'geometry': region.features[area].geometry
+				},
+				'generateId': true 
+            });
+            map.addLayer({
+                'id': layerName,
+                'type': 'fill',
+                'source': sourceName,
+                'layout': {},
+                'paint': {
+										'fill-color': color,
+										'fill-opacity': [
+											'case',
+											['boolean', ['feature-state', 'hover'], false],
+											1,
+											0.8
+											]
+                }
+						});
+						map.addLayer({
+							'id': layerNameBorders,
+							'type': 'line',
+							'source': sourceName,
+							'layout': {},
+							'paint': {
+									'line-color': '#666666',
+									'line-width': 0.8
+							}
+					});
+
+					map.on('click', layerName, function (region) {
+						new mapboxgl.Popup()
+						.setLngLat(region.lngLat)
+						.setHTML('<h6>' + risk_indicator  + '</h6>' + 
+						'<h5>'+'<strong>'+ risk[area][risk_value] + '</strong>'+'</h5>' +
+						'<h6>'+ layerName +'</h6>')
+						.addTo(map);
+						});
+
+					// When the user moves their mouse over the state-fill layer, we'll update the
+					// feature state for the feature under the mouse.
+					map.on('mousemove', layerName, function(region) {
+						map.getCanvas().style.cursor = 'pointer';
+						if (region.features.length > 0) {
+								if (hoveredStateId) {
+										map.setFeatureState({
+												source: sourceName,
+												id: hoveredStateId
+										}, {
+												hover: false
+										});
+								}
+								hoveredStateId = region.features[0].id;
+								map.setFeatureState({
+										source: sourceName,
+										id: hoveredStateId
+								}, {
+										hover: true
+								});
+						}
+					});
+
+					// this should disable hover effect when the mouse leaves but code 
+					// is not working
+					map.on('mouseleave', layerName, function() {
+						map.getCanvas().style.cursor = '';
+						if (hoveredStateId) {
+								map.setFeatureState({
+										source: sourceName,
+										id: hoveredStateId
+								}, {
+										hover: false
+								});
+						}
+						hoveredStateId = null;
+					});	
+				
+				};
+		
+				
