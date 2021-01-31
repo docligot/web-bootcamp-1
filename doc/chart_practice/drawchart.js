7497a00f44d7f8d79978c1f2afa56637b5e1fa6d
Dominic Ligot <dominic.ligot@gmail.com>
2021-01-31 20:26:40 +0800

    Update drawchart.js

--- doc/chart_practice/drawchart.js
+++ doc/chart_practice/drawchart.js
@@ -1,20 +1,25 @@
 function chartColors() {
 	window.chartColors = {
-	red: 'rgb(255, 99, 132)',
-	orange: 'rgb(255, 159, 64)',
-	yellow: 'rgb(255, 205, 86)',
-	green: 'rgb(75, 192, 192)',
-	blue: 'rgb(54, 162, 235)',
-	purple: 'rgb(153, 102, 255)',
-	grey: 'rgb(201, 203, 207)', 
-	black: 'rgb(0,0,0)'
-};
-
-	
-	
+		red: 'rgb(255, 99, 132)',
+		orange: 'rgb(255, 159, 64)',
+		yellow: 'rgb(255, 205, 86)',
+		green: 'rgb(75, 192, 192)',
+		blue: 'rgb(54, 162, 235)',
+		purple: 'rgb(153, 102, 255)',
+		grey: 'rgb(201, 203, 207)', 
+		black: 'rgb(0,0,0)'
+	};
 }
 
 function drawChart() {
+	chartColors();
 	
+	document.getElementById('chart_container').innerHTML = '<canvas id="myChart" style="height: 300px;"></canvas>';
 	
 }
+
+
+
+
+
+
