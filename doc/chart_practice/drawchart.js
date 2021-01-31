46ffa5c7cb67bfc50dc7b83b5d363a80662d1b5e
Dominic Ligot <dominic.ligot@gmail.com>
2021-01-31 21:01:24 +0800

    Update drawchart.js

--- doc/chart_practice/drawchart.js
+++ doc/chart_practice/drawchart.js
@@ -1,3 +1,15 @@
+function loadDoc() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "ajax_info.txt", true);
+	xhttp.send();
+}
+
+
 function chartColors() {
 	window.chartColors = {
 		red: 'rgb(255, 99, 132)',
